40238e4d06f6391fb7a0bcbc738e71bcd4295473
Vasyl-Pidgornyi <pidgornyi.vasyl@gmail.com>
2020-04-10 15:19:43 +0300

    trainer booking added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,10 @@
 import { Header } from "./header.js";
+import { trainerBooking } from "./trainerBooking.js";
 
 const pageContainer = document.querySelector(".page-container");
+
 const pageHeader = new Header(pageContainer);
 pageHeader.render();
+
+const bookTrainer = new trainerBooking(pageContainer);
+bookTrainer.render();
--- src/scripts/trainerBooking.js
+++ src/scripts/trainerBooking.js
@@ -7,5 +7,24 @@ export class trainerBooking {
     this.pageContentDiv = document.createElement("div");
     this.pageContentDiv.classList.add("trainerBooking-continer");
     this.rootElement.append(this.pageContentDiv);
+
+    this.trainerHeader = document.createElement("h1");
+    this.trainerDescription = document.createElement("p");
+    this.bookButton = document.createElement("a");
+
+    this.trainerHeader.classList.add("trainerBooking-continer__header");
+    this.trainerDescription.classList.add(
+      "trainerBooking-continer__description"
+    );
+    this.bookButton.classList.add("trainerBooking-continer__btn");
+
+    this.trainerHeader.textContent = "JOEY DIXON";
+    this.trainerDescription.textContent = "training with a world champion";
+    this.bookButton.textContent = "Book Now";
+    this.bookButton.href = " ";
+
+    this.pageContentDiv.append(this.trainerHeader);
+    this.pageContentDiv.append(this.trainerDescription);
+    this.pageContentDiv.append(this.bookButton);
   }
 }
